553cbb1c7d05c1b9c32b0ac948a6c7d38e71a761
thisChronomiaClock <b2029agnavarro@pshs.edu.ph>
2025-09-11 12:30:15 +0800

    The 10001 commit

---
+++ public/asterisk triangle.js
@@ -0,0 +1,15 @@
+let string = "";
+
+
+for (let i = 5; i >= 0; i--){
+    for (let j = i; j > 0; j--){
+        string += " ";
+    }
+    for (let k = 5 - i; k > 0; k--){
+        string += "*";
+    }
+    string += "\n";
+}
+   
+
+console.log(string);
